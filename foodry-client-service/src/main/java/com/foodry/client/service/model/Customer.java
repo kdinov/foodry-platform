d9e8ad0aa62b5fdddad6811d4a2b923c7f0648be
kalin.dinov <kalin.dinov@techhuddle.net>
2016-08-20 13:37:20 +0300

    Updated customer and shopper models

--- foodry-client-service/src/main/java/com/foodry/client/service/model/Customer.java
+++ foodry-client-service/src/main/java/com/foodry/client/service/model/Customer.java
@@ -12,27 +12,25 @@ public class Customer extends BaseAccount {
      * Constructor that sets all fields.
      * 
      * @param email the customer's email
-     * @param firstName the customer's first name
-     * @param lastName the customer's last name
+     * @param name the customer's name
      * @param phone the customer's phone
      * @param age the customer's age
      * @param passwordHash the customer's password hash
      * @param passwordSalt the customer's password salt
      */
-    public Customer(String email, String firstName, String lastName, String phone, int age, String passwordHash, String passwordSalt) {
-        super(email, firstName, lastName, phone, age, passwordHash, passwordSalt);
+    public Customer(String email, String name, String phone, int age, String passwordHash, String passwordSalt) {
+        super(email, name, phone, age, passwordHash, passwordSalt);
     }
 
     /**
      * Constructor that sets all fields.
      * 
      * @param email the customer's email
-     * @param firstName the customer's first name
-     * @param lastName the customer's last name
+     * @param name the customer's name
      * @param phone the customer's phone
      * @param age the customer's age
      */
-    public Customer(String email, String firstName, String lastName, String phone, int age) {
-        this(email, firstName, lastName, phone, age, null, null);
+    public Customer(String email, String name, String phone, int age) {
+        this(email, name, phone, age, null, null);
     }
 }
